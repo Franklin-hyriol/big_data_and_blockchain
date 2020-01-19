1e154625543f4d52638f00729e6473722c2365fa
franklin-hyriol <franklinrazafy@gmail.com>
2020-01-19 22:27:07 +0300

    desole pour le retard

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,96 +1,193 @@
-
+#le big data
 big_data = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
-nombre_journaliste = 0
-handle_numero = 0
 
-big_data.each do |name|
-	nombre_journaliste += 1
+def question_un(big_data)
+	nombre_journaliste = 0
+
+
+	big_data.each do |name|
+		nombre_journaliste += 1
+	end
+
+	puts "nombre de journaliste: #{nombre_journaliste}"
 end
 
-puts "nombre journaliste: #{nombre_journaliste}"
+#---------------------------------------------------------------#
 
-big_data.each do |name|
-	for i in 0..9
-		if name.include? i.to_s
-			handle_numero += 1
+def question_deux(big_data)
+	handle_numero = 0
+	big_data.each do |name|
+		for i in 0..9
+			if name.include? i.to_s
+				handle_numero += 1
+			end
 		end
 	end
+
+	puts "nombre d'handle qui contiennent un numero: #{handle_numero}"
 end
 
-puts "nombre d'handle qui contiennent un numero: #{handle_numero}"
+#-----------------------------------------------------------------#
 
-a = 0
-u = 0
-d = 0
-e = 0
-aude = 0
+def question_trois(big_data)
 
-big_data.each do |name|
-	name.split('').each do |char|
-		if char.downcase == "a" && a == 0 && u == 0 && d == 0 && e == 0
-			a = 1
-		end
+	a = 0
+	u = 0
+	d = 0
+	e = 0
+	aude = 0 #nombre d'aude
 
-		if char.downcase == "u" && u == 0 && a == 1 && d == 0 && e == 0
-			u = 1
-		end
+	big_data.each do |name|
+		name.split('').each do |char|
+			if char.downcase == "a" && a == 0 && u == 0 && d == 0 && e == 0
+				a = 1
+			end
 
-		if char.downcase == "d" && d == 0 && a == 1 && u == 1 && e == 0
-			d = 1
-		end
+			if char.downcase == "u" && u == 0 && a == 1 && d == 0 && e == 0
+				u = 1
+			end
 
-		if char.downcase == "e" && e == 0 && a == 1 && u == 1 && d == 1
-			e = 1
-		end
+			if char.downcase == "d" && d == 0 && a == 1 && u == 1 && e == 0
+				d = 1
+			end
 
-		if a == 1 && u == 1 && d == 1 && e == 1
-			aude += 1
-			a = 0
-			u = 0
-			d = 0
-			e = 0
+			if char.downcase == "e" && e == 0 && a == 1 && u == 1 && d == 1
+				e = 1
+			end
+
+			if a == 1 && u == 1 && d == 1 && e == 1
+				aude += 1
+				a = 0
+				u = 0
+				d = 0
+				e = 0
+			end
 		end
 	end
+	puts "nombre d'handle contiennent les 4 lettres du prenom Aude a la suite: #{aude}"
 end
 
-puts "nombre d'aude: #{aude}"
+#--------------------------------------------------------------------------#
 
 
-nom_maj = 0
+def question_quatre(big_data)
+	nom_maj = 0
+	big_data.each do |name|
+		for i in 'A'..'Z'
+			if name[1] == i
+				nom_maj += 1
+			end
+		end
+			
+	end
+	puts "nombre de nom qui commencent par une majuscule : #{nom_maj}"
+end
 
+#----------------------------------------------------------------------------#
 
-big_data.each do |name|
-	for i in 'A'..'Z'
-		if name[1] == i
-			nom_maj += 1
+def question_cinq(big_data)
+	nbr_maj_nom = 0
+	j = 0
+
+	big_data.each do |name|
+		long = name.length
+		for i in 'A'..'Z'
+			for j in 0..long	
+			   if name[j] == i
+			   		nbr_maj_nom += 1
+			   		break
+			   	end
+			end
+			break if name[j] == i
 		end
 	end
-		
+
+	puts "nombre de nom qui contiennent une majuscule : #{nbr_maj_nom}"
+
 end
+
+#------------------------------------------------------------------------------#
+
+def question_six(big_data)
+	nbr_de_tiree = 0
+
+	big_data.each do |name|
+		long = name.length
+		for i in 0..long
+			if name[i] == "_"
+				nbr_de_tiree += 1
+			end
+		end
+	end
+
+	puts "nombre de _ dans tous les pseudos confondus: #{nbr_de_tiree}"
+end
+
+#-----------------------------------------------------------------------#
+
+def question_sept(big_data)
+	big_data.sort!
+	puts "nom triee par ordre alphabetique"
+	puts big_data
+end
+
+#------------------------------------------------------------------------#
+
+def question_huit(big_data)
+	les_50_nom_cours = []
+	name_long = []
+
+	big_data.each do |name|
+		name_long << name.length
+	end
+
+	name_long.sort!
+	nombre = 49
+
+	big_data.each do |name|
+	 	if name.length <=  name_long[49]
+	 		les_50_nom_cours << name
+	 		nombre -=1
+	 	end
+	 	break if nombre <= 0
+	end
+
+	puts "les 50 nom les plus cours" 
+	puts les_50_nom_cours
+end
+
+#-----------------------------------------------------------------------#
+
+def question_neuf(big_data)
+	position_epenser = 0
+	position = 1
+
+	big_data.each do |name|
+		if name == "@epenser"
+			position_epenser = position
+			break
+		end
+		position+=1
+	end
+	puts "position de @epenser: #{position_epenser}"
+end
+
+
+#-------------------------------------------------------------------------#
+
+question_un(big_data)
+question_deux(big_data)
+question_trois(big_data)
+question_quatre(big_data)
+question_cinq(big_data)
+question_six(big_data)
+
+=begin #decommenter pour voir les resultat 
 	
-	puts "nom qui commence par MAJ : #{nom_maj}"
-nbr_maj_nom = 0
+question_sept(big_data)
+question_huit(big_data)
+	
+=end #cause du commentaire => reponse tros long :D
 
-big_data.each do |name|
-	long = name.length
-	for i in 'A'..'Z'
-		 until long == 0
-		 	long -=1
-		   break if name[long] == i
-		 end
-		 
-		break if name[long] == i
-	end
-
-end
-
-puts "nombre de nom qui des MAJ : #{nbr_maj_nom}"
-
-
-
-
-
-
-
-
+question_neuf(big_data)
